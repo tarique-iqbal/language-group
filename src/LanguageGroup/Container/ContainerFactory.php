ce2876c34c617232a3f72f4c1f6ad6eec57af8b2
Tarique Iqbal <live.iqbal@gmail.com>
2021-11-15 20:27:43 +0100

    Implement ContainerFactory class

---
+++ src/LanguageGroup/Container/ContainerFactory.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace LanguageGroup\Container;
+
+use LanguageGroup\Service\ConfigService;
+use LanguageGroup\Service\CurlService;
+use Pimple\Container;
+
+/**
+ * Class ContainerFactory
+ * @package LanguageGroup\Container
+ */
+class ContainerFactory
+{
+    /**
+     * @var array
+     */
+    private array $config;
+
+    /**
+     * ContainerFactory constructor.
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return Container
+     */
+    public function create(): Container
+    {
+        $container = new Container();
+
+        $container['ConfigService'] = function () {
+            return new ConfigService($this->config);
+        };
+
+        $container['CurlService'] = function () {
+            return new CurlService();
+        };
+
+        return $container;
+    }
+}
---
+++ tests/Integration/Container/ContainerFactoryTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration\Container;
+
+use LanguageGroup\Container\ContainerFactory;
+use LanguageGroup\Service\ConfigService;
+use LanguageGroup\Service\CurlService;
+use PHPUnit\Framework\TestCase;
+use Pimple\Container;
+
+class ContainerFactoryTest extends TestCase
+{
+    public function testCreate(): void
+    {
+        $config = include BASE_DIR . '/config/parameters_test.php';
+
+        $container = (new ContainerFactory($config))->create();
+
+        $this->assertInstanceOf(Container::class, $container);
+        $this->assertInstanceOf(ConfigService::class, $container['ConfigService']);
+        $this->assertInstanceOf(CurlService::class, $container['CurlService']);
+    }
+}
